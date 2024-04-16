2afef247c792dcdafbf887c983876f267ea872c2
jiesir <jiesir16@gmail.com>
2024-04-16 20:41:57 +0800

    fix:调试个人页面

--- src/views/frontViews/Person.vue
+++ src/views/frontViews/Person.vue
@@ -1,130 +1,128 @@
 <template>
-  <n-flex vertical align="center">
-    <n-card
-      style="
-        max-width: 80%;
-        background-color: #ffffff;
-        padding: 20px;
-        min-height: 85dvh;
-      "
-    >
-      <n-flex vertical>
-        <n-flex vertical align="center">
-          <img
-            alt="11"
-            src="https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif?imageView2/1/w/80/h/80"
-            style="border-radius: 50%"
-          />
-          <n-text
-            >{{
-              useUsersStore().loginUserInfo.name
-                ? useUsersStore().loginUserInfo.name
-                : useUsersStore().loginUserInfo.username
-            }}
-          </n-text>
-          <n-text>{{ useUsersStore().loginUserInfo.department }}</n-text>
-          <n-text
-            >{{
-              useUsersStore().loginUserInfo.grade +
-              useUsersStore().loginUserInfo.profession +
-              useUsersStore().loginUserInfo.userClass
-            }}班
-          </n-text>
-        </n-flex>
-
-        <n-flex style="min-height: 61dvh">
-          <n-tabs
-            type="line"
-            animated
-            :tabs-padding="12"
-            default-value="collected"
-            :value="selectedRef"
-            @update:value="handleChangeTab"
-          >
-            <n-tab-pane
-              name="collected"
-              :tab="renderTab('i-BookmarkOutline', '收藏的活动')"
-              display-directive="show:lazy"
-            >
-              <n-grid x-gap="12" y-gap="8" :cols="3">
-                <n-gi v-for="(item, index) in activitiesRef">
-                  <n-card
-                    style="cursor: pointer"
-                    :title="item.title"
-                    @click="handleClick(index)"
-                  >
-                    <template #cover>
-                      <div style="height: 200px">
-                        <img :src="item.imgUrl" :alt="item.title" />
-                      </div>
-                    </template>
-                    {{ item.desc }}
-                    {{ index }}
-                  </n-card>
-                </n-gi>
-              </n-grid>
-            </n-tab-pane>
-            <n-tab-pane
-              name="enrolled"
-              :tab="renderTab('i-TicketOutline', '参与过的活动')"
-              display-directive="show:lazy"
-            >
-              <n-grid x-gap="12" y-gap="8" :cols="3">
-                <n-gi v-for="(item, index) in activitiesRef">
-                  <n-card
-                    style="cursor: pointer"
-                    :title="item.title"
-                    @click="handleClick(index)"
-                  >
-                    <template #cover>
-                      <div style="height: 200px">
-                        <img :src="item.imgUrl" :alt="item.title" />
-                      </div>
-                    </template>
-                    {{ item.desc }}
-                    {{ index }}
-                  </n-card>
-                </n-gi>
-              </n-grid>
-            </n-tab-pane>
-            <n-tab-pane
-              name="published"
-              :tab="renderTab('i-SendOutline', '发布过的活动')"
-              display-directive="show:lazy"
-            >
-              <n-grid x-gap="12" y-gap="8" :cols="3">
-                <n-gi v-for="(item, index) in activitiesRef">
-                  <n-card
-                    style="cursor: pointer"
-                    :title="item.title"
-                    @click="handleClick(index)"
-                  >
-                    <template #cover>
-                      <div style="height: 200px">
-                        <img :src="item.imgUrl" :alt="item.title" />
-                      </div>
-                    </template>
-                    {{ item.desc }}
-                    {{ index }}
-                  </n-card>
-                </n-gi>
-              </n-grid>
-            </n-tab-pane>
-          </n-tabs>
-        </n-flex>
+  <n-card
+    style="
+      max-width: 1383px;
+      background-color: #ffffff;
+      padding: 20px;
+      min-height: 85dvh;
+    "
+  >
+    <n-flex vertical>
+      <n-flex vertical align="center">
+        <img
+          alt="11"
+          src="https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif?imageView2/1/w/80/h/80"
+          style="border-radius: 50%"
+        />
+        <n-text
+          >{{
+            useUsersStore().loginUserInfo.name
+              ? useUsersStore().loginUserInfo.name
+              : useUsersStore().loginUserInfo.username
+          }}
+        </n-text>
+        <n-text>{{ useUsersStore().loginUserInfo.department }}</n-text>
+        <n-text
+          >{{
+            useUsersStore().loginUserInfo.grade +
+            useUsersStore().loginUserInfo.profession +
+            useUsersStore().loginUserInfo.userClass
+          }}班
+        </n-text>
       </n-flex>
-      <template #footer>
-        <n-flex justify="center">
-          <n-pagination
-            :page="page"
-            :page-count="pages"
-            @update:page="handleUpdatePage"
-            simple
-          />
-        </n-flex>
-      </template>
-    </n-card>
-  </n-flex>
+
+      <n-flex style="min-height: 61dvh">
+        <n-tabs
+          type="line"
+          animated
+          v-model:value="selectedRef"
+          :tabs-padding="12"
+          default-value="collected"
+          @update:value="handleChangeTab"
+        >
+          <n-tab-pane
+            name="collected"
+            :tab="renderTab('i-BookmarkOutline', '收藏的活动')"
+            display-directive="show:lazy"
+          >
+            <n-grid x-gap="12" y-gap="8" :cols="3">
+              <n-gi v-for="(item, index) in activitiesRef">
+                <n-card
+                  style="cursor: pointer"
+                  :title="item.title"
+                  @click="handleClick(index)"
+                >
+                  <template #cover>
+                    <div style="height: 200px">
+                      <img :src="item.imgUrl" :alt="item.title" />
+                    </div>
+                  </template>
+                  {{ item.desc }}
+                  {{ index }}
+                </n-card>
+              </n-gi>
+            </n-grid>
+          </n-tab-pane>
+          <n-tab-pane
+            name="enrolled"
+            :tab="renderTab('i-TicketOutline', '参与过的活动')"
+            display-directive="show:lazy"
+          >
+            <n-grid x-gap="12" y-gap="8" :cols="3">
+              <n-gi v-for="(item, index) in activitiesRef">
+                <n-card
+                  style="cursor: pointer"
+                  :title="item.title"
+                  @click="handleClick(index)"
+                >
+                  <template #cover>
+                    <div style="height: 200px">
+                      <img :src="item.imgUrl" :alt="item.title" />
+                    </div>
+                  </template>
+                  {{ item.desc }}
+                  {{ index }}
+                </n-card>
+              </n-gi>
+            </n-grid>
+          </n-tab-pane>
+          <n-tab-pane
+            name="published"
+            :tab="renderTab('i-SendOutline', '发布过的活动')"
+            display-directive="show:lazy"
+          >
+            <n-grid x-gap="12" y-gap="8" :cols="3">
+              <n-gi v-for="(item, index) in activitiesRef">
+                <n-card
+                  style="cursor: pointer"
+                  :title="item.title"
+                  @click="handleClick(index)"
+                >
+                  <template #cover>
+                    <div style="height: 200px">
+                      <img :src="item.imgUrl" :alt="item.title" />
+                    </div>
+                  </template>
+                  {{ item.desc }}
+                  {{ index }}
+                </n-card>
+              </n-gi>
+            </n-grid>
+          </n-tab-pane>
+        </n-tabs>
+      </n-flex>
+    </n-flex>
+    <template #footer>
+      <n-flex justify="center">
+        <n-pagination
+          :page="page"
+          :page-count="pages"
+          @update:page="handleUpdatePage"
+          simple
+        />
+      </n-flex>
+    </template>
+  </n-card>
 </template>
 <script setup lang="ts">
 import { ref } from "vue";
@@ -141,10 +139,11 @@ interface GridItem {
 
 const page = ref(1);
 const pages = ref(1);
-const selectedRef = ref<String>();
+const selectedRef = ref<String>("collected");
 const activitiesRef = ref<Array<GridItem>>([]);
 
 function handleUpdatePage(pageNo) {
+  console.log("[pageNo]", pageNo, selectedRef);
   pageNo = pageNo ? pageNo : 1;
   handleChangeTab(selectedRef.value, pageNo);
 }
@@ -154,6 +153,7 @@ function handleClick(index: any) {
 }
 
 function handleChangeTab(tableName: any, pageNo?: number) {
+  console.log("[handleChangeTab]", tableName, pageNo);
   if ("collected" === tableName) {
     fetchActivities({ size: 6, current: pageNo, collected: true });
   } else if ("enrolled" === tableName) {
@@ -176,5 +176,5 @@ function fetchActivities(queryParam) {
   });
 }
 
-handleUpdatePage(selectedRef.value);
+fetchActivities({ size: 6, current: 1, collected: true });
 </script>
